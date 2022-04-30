6daed4ef386035b13f73945722c14b0877d9e7e2
Gortyui <gorvibranium07@gmail.com>
2022-04-30 18:53:31 +0400

    Updates

---
+++ static/js/4.93f938ef.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpreact1=this.webpackJsonpreact1||[]).push([[4],{91:function(a,e,t){a.exports={item:"about_us_item__1XPPj"}},97:function(a,e,t){"use strict";t.r(e);t(1);var n=t(91),c=t.n(n),i=t(0);e.default=function(a){return Object(i.jsx)("div",{className:"navigacia",children:Object(i.jsx)("div",{className:c.a.item,children:"Nerka shamanak proekti vra ashxatum e mek hogi ev ayn mec ce bayc herankarum ayn @ndlaynvelu e"})})}}}]);
+//# sourceMappingURL=4.93f938ef.chunk.js.map
